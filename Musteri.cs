68f6b6dce8a8242d8989da8982a055f6531d845a
icivioglu <icivioglu@gmail.com>
2021-01-28 16:58:00 +0300

    Musteri, MusteriManager sınıfları eklendi.

---
+++ Musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Musteri
+{
+	public Musteri()
+	{
+		public int MusteriId { get; set; }
+		public string MusteriAdi { get; set; }
+		public string MusteriSoyadi { get; set; }
+		public int MusteriSifre { get; set; }
+	}
+}
---
+++ MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class MusteriManager
+{
+	public MusteriManager()
+	{
+		
+	}
+	public void Ekleme (Musteri musteri)
+    {
+		Console.WriteLine("Tebrikler. Müşteri eklendi" + musteri.MusteriAdi)
+    }
+	public void Silme(Musteri musteri)
+	{
+		Console.WriteLine("Tebrikler. Müşteri eklendi" + musteri.MusteriAdi)
+
+	}
+	public void Listeleme(Musteri musteri)
+	{
+		Console.WriteLine("Tebrikler. Müşteri eklendi" + musteri.MusteriAdi)
+
+	}
+
+}
